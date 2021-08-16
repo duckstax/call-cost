da157d2542e2995d1987e10c2b6afc9ea1d6e71c
kotbegemot <k0tb9g9m0t@yandex.ru>
2021-08-16 10:18:21 +0300

    init

--- python.cpp
+++ python.cpp
@@ -21,12 +21,15 @@ struct data final {
 PYBIND11_EMBEDDED_MODULE(fast_calc, m) {
     m.def("print_data", [](data &self) -> void {
         std::cout << "data { id: " << self.id << " name " << self.name << " }"
-                  << std::endl;
+                  <<
+                  std::endl;
     });
 
-    m.def("rbp", [](int a, int b) -> int { return a + b; });
+    m.def("rbp", [](int a,int b) -> int {
+        return a +b;
+    });
 
-    py::class_<data>(m, "Data")
+    py::class_<data>(m,"Data")
             .def_readwrite("id", &data::id)
             .def_readwrite("name", &data::name)
             .def_readwrite("last_name", &data::last_name)
@@ -37,63 +40,39 @@ PYBIND11_EMBEDDED_MODULE(fast_calc, m) {
 }
 
 
-
-constexpr static char script[] = R"__(
-        #import sys, os
-        #from importlib import import_module
-        #sys.modules['fast_calc'] = fast_calc
-
-        from fast_calc import  rbp
-
-        def run(a,b):
-            return rbp(a, b)
-
-        def fcn2( a  ):
-            a.id  = a.id2
-            return a
-)__";
-
-
 static void bm_python_invoke_with_binded(benchmark::State &state) {
     py::scoped_interpreter guard{};
-
-    ///py::eval_file("script.py", py::globals(), py::dict());
-
-    auto script = py::module_::import("script.py");
-   // auto run = script.attr("run");
-
-    //for (auto _ : state) {
-
-//        int abc = run(1, 2).cast<int>();;
-  //  }
+    auto script = py::module_::import("script");
+    for (auto _ : state) {
+        auto run = script.attr("run");
+        int abc = run(1, 2).cast<int>();;
+    }
 }
-/*
-static void bm_python_invoke_with_usertype(benchmark::State& state) {
-    py::scoped_interpreter guard{};
-    lua.open_libraries(sol::lib::base);
-    lua.script_file("script.lua");
 
+static void bm_python_invoke_with_usertype(benchmark::State &state) {
+    py::scoped_interpreter guard{};
+    auto script = py::module_::import("script");
+/*
     auto bark = lua["bark"].get_or_create<sol::table>();
     bark.new_usertype<data>("data", "id", &data::id, "name", &data::name,
                             "last_name", &data::last_name, "hosname",
                             &data::hostname, "id2", &data::id2, "hosname2",
                             &data::hostname2);  // the usual
 
-                            bark.set_function("print_data", [](data& self) {
-                                std::cout << "data { id: " << self.id << " name " << self.name << " }"
-                                << std::endl;
-                            });
-
-                            for (auto _ : state) {
-                                sol::function f = lua["fcn2"];
-                                data abc = f(data{1, "mnm"});
-                            }
+    bark.set_function("print_data", [](data &self) {
+        std::cout << "data { id: " << self.id << " name " << self.name << " }"
+                  << std::endl;
+    });
+    */
+    for (auto _ : state) {
+        auto fcn2 = script.attr("fcn2");
+        data abc = fcn2(data{1, "mnm"}).cast<data>();
+    }
 }
-*/
+
 // Register the function as a benchmark
 BENCHMARK(bm_python_invoke_with_binded);
-/*
 BENCHMARK(bm_python_invoke_with_usertype);
-*/
+
 // Run the benchmark
 BENCHMARK_MAIN();
